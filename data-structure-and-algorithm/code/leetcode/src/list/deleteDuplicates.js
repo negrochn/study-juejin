eabb91ef586487f05d91481035f14fb411bd6fba
negrochn <negrochn7@gmail.com>
2020-11-17 14:08:59 +0800

    链表的应用—真题归纳与解读

--- data-structure-and-algorithm/code/leetcode/src/list/deleteDuplicates.js
+++ data-structure-and-algorithm/code/leetcode/src/list/deleteDuplicates.js
@@ -7,23 +7,32 @@ import ListNode from './listNode'
  * @param {ListNode} head 
  */
 export default function deleteDuplicates(head) {
+  // 极端情况：0 个或 1 个节点，则不会重复，直接返回
   if (!head || !head.next) {
     return head
   }
+  // dummy 结点
   let dummy = new ListNode()
+  // dummy 永远指向头结点
   dummy.next = head
+  // cur 从 dummy 开始遍历
   let cur = dummy
+  // 当 cur 的后面有至少两个节点时
   while(cur.next && cur.next.next) {
+    // 对 cur 后面的两个节点的 val 进行比较
     if (cur.next.val === cur.next.next.val) {
+      // 若值重复，则记下这个值
       let val = cur.next.val
+      // 反复地排查后面的元素是否存在多次重复该值的情况
       while(cur.next && cur.next.val === val) {
+        // 若有，则删除
         cur.next = cur.next.next
       }
     } else {
+      // 若不重复，则正常遍历
       cur = cur.next
     }
   }
-  // console.log(head)
-  return head
-  // return dummy.next
+  // 返回链表的起始节点
+  return dummy.next
 }
